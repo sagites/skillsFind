96dcbd4d24c4ad8684465dff2571f0fb3f5f4add
sagites <chizzyikemba@gmail.com>
2025-04-02 09:38:34 +0100

    another restructuring

--- routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {Signup, Login} = require('../controller/authController')
-
-router.post('/signup', Signup)
-router.post('/login', Login)
-
-module.exports = router;
--- routes/verifyEmail.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { verifyEmailMail, resendEmail} = require("../controller/verifyEmailController");
-
-router.post('/verifyEmail', verifyEmailMail);
-router.post('/resend-email', resendEmail);
-
-module.exports = router;
-

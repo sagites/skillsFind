0949975bdbb60bf7466768514b2517c29785a9ea
sagites <chizzyikemba@gmail.com>
2025-04-02 09:36:20 +0100

    restructured the codebase

--- utils/generateToken.js
+++
@@ -1,7 +0,0 @@
-const crypto = require("crypto");
-
-const generateToken = () => {
-    return crypto.randomInt(100000, 999999).toString();
-};
-
-module.exports = generateToken;
--- utils/handleError.js
+++
@@ -1,5 +0,0 @@
-const handleErrorResponse = (res, statusCode, message) => {
-  res.status(statusCode).json({ success: false, message });
-};
-
-module.exports = { handleErrorResponse };

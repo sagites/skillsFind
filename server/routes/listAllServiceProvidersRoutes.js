0bf3fb5b31eeb58afa9d2c572f82dd0a3eeda51b
sagites <chizzyikemba@gmail.com>
2025-07-29 19:58:21 +0100

    added the getServiceProvider to the routes

--- server/routes/listAllServiceProvidersRoutes.js
+++ server/routes/listAllServiceProvidersRoutes.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
 const {
   listAllServiceProviders,
+  getServiceProvider,
 } = require("../controller/users/listAllServiceProviders");
 const protect = require("../middleware/protect");
 
-router.route("/").get(protect, listAllServiceProviders);
+router
+  .route("/")
+  .get(protect, listAllServiceProviders)
+  .post(protect, getServiceProvider);
 
 module.exports = router;

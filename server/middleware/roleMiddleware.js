86bea91ddd52378d4e40eb19b3bb60c48b598095
sagites <chizzyikemba@gmail.com>
2025-07-30 19:43:22 +0100

    created a role middleware

--- server/middleware/roleMiddleware.js
+++ server/middleware/roleMiddleware.js
@@ -1,10 +1,15 @@
-const authorize = (...roles) => {
+const authorize = (...allowedTypes) => {
   return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
+    if (!req.accountType) {
+      return res.status(401).json({ message: "Unauthorized: No account type found" });
+    }
+
+    if (!allowedTypes.includes(req.accountType)) {
       return res.status(403).json({ message: "Forbidden: You do not have access" });
     }
+
     next();
   };
 };
 
-module.exports = { authorize };
+module.exports = {authorize};

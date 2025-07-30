9e24478ee63aed3150cc0bacfb3ba76f9be451e3
sagites <chizzyikemba@gmail.com>
2025-07-30 19:46:00 +0100

    Added both the vendor and the user to be authorized in the middleware

--- server/routes/updateProfileRoutes.js
+++ server/routes/updateProfileRoutes.js
@@ -3,6 +3,6 @@ const protect = require("../middleware/protect");
 const { authorize } = require("../middleware/roleMiddleware");
 const updateProfile = require("../controller/updateProfile");
 
-router.route("/").put(protect, authorize("vendor"), updateProfile);
+router.route("/").put(protect, authorize("vendor", "user"), updateProfile);
 
 module.exports = router;

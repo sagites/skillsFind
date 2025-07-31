ba2126f585facff9a580937be7bec304ad759eba
sagites <chizzyikemba@gmail.com>
2025-07-31 19:16:27 +0100

    Fixed the vendor is not defined by importing it

--- server/models/review.js
+++ server/models/review.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Vendor = require("./Vendor");
 
 const reviewSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },

6bbee6e070986b3cdb771ff27e3580f226151639
sagites <chizzyikemba@gmail.com>
2025-07-31 22:45:09 +0100

    Added the addAdmin and deleteAdmin endpoints

--- server/controller/adminController.js
+++ server/controller/adminController.js
@@ -2,8 +2,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const Admin = require("../models/admin");
 const asyncHandler = require("express-async-handler");
-const {handleErrorResponse} = require("../utils/handleError");
-const expressAsyncHandler = require("express-async-handler");
+const { handleErrorResponse } = require("../utils/handleError");
 const { hashPassword, checkPassword } = require("../utils/hassPassword");
 
 const adminSignup = asyncHandler(async (req, res) => {
@@ -83,15 +82,66 @@ const adminLogin = asyncHandler(async (req, res) => {
 
 const addNewAdmin = asyncHandler(async (req, res) => {
   try {
+    const { name, email, password, confirmPassword, role } = req.body;
+
+    if (!name || !email || !password || !confirmPassword || !role) {
+      return res.status(400).json({ message: "Please input all fields" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password should be at least 6 characters long" });
+    }
+
+    if (confirmPassword !== password) {
+      return res.status(400).json({ message: "Passwords do not match" });
+    }
+
+    const existingAdmin = await Admin.findOne({ email });
+    if (existingAdmin) {
+      return res.status(409).json({ message: "Email is already registered" });
+    }
+
+    const hashedPassword = await hashPassword(password);
+    const newAdmin = new Admin({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    await newAdmin.save();
+    res.status(201).json({ message: "New admin created successfully" });
   } catch (error) {
     console.error("Create Admin Error:", error);
     handleErrorResponse(res, 500, error.message);
   }
 });
 
+const deleteAdmin = asyncHandler(async (req, res) => {
+  try {
+    const { adminId } = req.params;
+    const admin = await Admin.findById(adminId);
+    if (!admin) {
+      return handleErrorResponse(res, 404, "Admin not found");
+    }
+    admin.isActive = false;
+    await admin.save();
+    res
+      .status(200)
+      .json({ success: true, message: "Admin deleted successfully" });
+  } catch (error) {
+    handleErrorResponse(res, 500, error.message);
+  }
+});
+
 const getAllUsers = asyncHandler(async (req, res) => {
   try {
-    const users = await User.find({}, { username: 1, email: 1, _id: 0 });
+    const users = await User.find(
+      { isDeleted: false },
+      { username: 1, email: 1, _id: 0 }
+    );
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -100,6 +150,8 @@ const getAllUsers = asyncHandler(async (req, res) => {
 
 module.exports = {
   adminSignup,
+  addNewAdmin,
+  deleteAdmin,
   adminLogin,
   getAllUsers,
 };
--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -4,11 +4,15 @@ const protect = require("../middleware/protect");
 const { authorize } = require("../middleware/roleMiddleware");
 const {
   adminSignup,
+  addNewAdmin,
   adminLogin,
+  deleteAdmin,
   getAllUsers,
 } = require("../controller/adminController");
 
 router.route("/users/").get(protect, authorize(ROLES.ADMIN, ROLES.SUPERADMIN), getAllUsers);
 router.post("/login", adminLogin).post("/signup", adminSignup);
+router.post("/add-admin", protect, authorize(ROLES.SUPERADMIN), addNewAdmin);
+router.delete("/remove-admin/:adminId", protect, authorize(ROLES.SUPERADMIN), deleteAdmin);
 
 module.exports = router;

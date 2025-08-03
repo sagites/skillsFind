d0e1f92d4818f2b78c3b3a0c1284e2af9efd84be
sagites <chizzyikemba@gmail.com>
2025-08-03 14:57:10 +0100

    Implemented a create error utility

--- server/utils/handleError.js
+++ server/utils/handleError.js
@@ -1,5 +1,16 @@
-const handleErrorResponse = (res, statusCode, message) => {
-  res.status(statusCode).json({ success: false, message });
+const createError = require("./createError");
+
+/**
+ * Wrapper to create and pass error to the next middleware
+ * @param {Object} res - Express response object
+ * @param {Object} next - Express next function
+ * @param {number} statusCode - HTTP status code
+ * @param {string} message - Error message
+ */
+const handleErrorResponse = (res, next, statusCode, message) => {
+  return next(createError(message, statusCode));
 };
 
 module.exports = { handleErrorResponse };
+
+
